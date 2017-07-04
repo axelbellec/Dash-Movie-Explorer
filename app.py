acd386db206e319c5c9a3524d899830c1f3ca0a7
Axel Bellec <axel.bellec@outlook.fr>
2017-07-04 22:50:04 +0200

    add all RangeSliders and Dropdown menu selection

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-from get_data import all_movies, variable_labels
+from get_data import all_movies, variable_labels, genres
 
+# TODO: add Flask-Caching using Redis as backend db
+
+# Access data
 df = all_movies.copy()
 
+# Init Dash app
 app = dash.Dash()
 
-
+# Set app layout
 app.layout = html.Div([
     html.H1('Movie Explorer'),
     html.H2('Dash: A web application framework for Python.'),
@@ -25,13 +29,12 @@ app.layout = html.Div([
         ),
         html.Br(),
         html.Label('Year released'),
-        dcc.Slider(
-            id='year-released-slider',
+        dcc.RangeSlider(
+            id='year-released-range-slider',
             min=df.Year.min(),
             max=df.Year.max(),
             marks={str(y): str(y) for y in range(int(df.Year.min()), int(df.Year.max()), 5)},
-            value=df.Year.min(),
-            step=None
+            value=[df.Year.min(), df.Year.max()]
         ),
         html.Br(),
         html.Label('Minimum number of Oscar wins (all categories)'),
@@ -43,8 +46,62 @@ app.layout = html.Div([
             value=df.Oscars.min(),
             step=None
         ),
-        html.Br()], style={'marginLeft': 25, 'marginRight': 25}
+        html.Br(),
+        html.Label('Dollars at Box Office (millions)'),
+        dcc.RangeSlider(
+            id='dollars-boxoffice-range-slider',
+            min=0,
+            max=800,
+            marks={str(y): str(y) for y in range(0, 800, 20)},
+            value=[0, 800],
+        ),
+        html.Br(),
+        html.Label('Genre (a movie can have multiple genres)'),
+        dcc.Dropdown(
+            id='genre-dropdown',
+            options=[
+                {'label': genre.upper(), 'value': genre} for genre in genres
+            ],
+            multi=False
+        ),
+        html.Br(),
+        html.Label('Director name contains (e.g., Miyazaki)'),
+        dcc.Input(id='director-input', value='', type='text'),
+        html.Br(),
+        html.Label('Cast names contains (e.g. Tom Hanks)'),
+        dcc.Input(id='cast-input', value='', type='text'),
+        html.Br(),
+    ], style={'marginLeft': 25, 'marginRight': 25}
     ),
+    html.Div([
+        html.Br(),
+        html.Label('X-axis variable'),
+        dcc.Dropdown(
+            id='x-axis-dropdown',
+            options=[
+                {'label': label, 'value': value} for value, label in variable_labels.items()
+            ],
+            multi=False,
+            value='Meter'
+        ),
+        html.Br(),
+        html.Label('Y-axis variable'),
+        dcc.Dropdown(
+            id='y-axis-dropdown',
+            options=[
+                {'label': label, 'value': value} for value, label in variable_labels.items()
+            ],
+            multi=False,
+            value='Reviews'
+        ),
+        html.Br(),
+        html.P('''
+        Note: The Tomato Meter is the proportion of positive reviews (as judged by the Rotten Tomatoes staff),
+        and the Numeric rating is a normalized 1-10 score of those reviews which have star ratings (for example,
+        3 out of 4 stars).
+        '''),
+        html.Br()
+    ], style={'marginLeft': 25, 'marginRight': 25}),
     html.Div([
         html.Label('Graph'),
         dcc.Graph(
@@ -86,7 +143,9 @@ app.layout = html.Div([
                     hovermode='closest'
                 )
             }
-        )
+        ),
+        html.Br(),
+        html.P('Number of rows selected: {}'.format(len(df.index)), id='dataset-rows-p')
     ], style={'marginLeft': 25, 'marginRight': 25})
 ])
 
@@ -95,28 +154,47 @@ app.layout = html.Div([
     dash.dependencies.Output('scatter-plot-graph', 'figure'),
     [
         dash.dependencies.Input('nb-reviews-slider', 'value'),
-        dash.dependencies.Input('year-released-slider', 'value'),
-        dash.dependencies.Input('oscars-won-slider', 'value')
+        dash.dependencies.Input('year-released-range-slider', 'value'),
+        dash.dependencies.Input('oscars-won-slider', 'value'),
+        dash.dependencies.Input('dollars-boxoffice-range-slider', 'value'),
+        dash.dependencies.Input('genre-dropdown', 'value'),
+        dash.dependencies.Input('director-input', 'value'),
+        dash.dependencies.Input('cast-input', 'value'),
+        dash.dependencies.Input('x-axis-dropdown', 'value'),
+        dash.dependencies.Input('y-axis-dropdown', 'value')
     ]
 )
-def update_scatter_plot(selected_nb_reviews, selected_year_released, selected_nb_oscars_won):
-
-    print(selected_nb_reviews, selected_year_released, selected_nb_oscars_won)
+def update_scatter_plot(selected_nb_reviews, selected_years_released, selected_nb_oscars_won,
+                        selected_dollars_boxoffice, selected_genre, input_director, input_cast,
+                        x_axis_var, y_axis_var):
 
     nb_reviews = selected_nb_reviews or df.Reviews.min()
-    year_released = selected_year_released or df.Year.min()
+    year_released_start, year_released_end = selected_years_released or (df.Year.min(), df.Year.max())
     oscars_won = selected_nb_oscars_won or df.Oscars.min()
+    dollars_boxoffice_min, dollars_boxoffice_max = (amount * 1e6 for amount in selected_dollars_boxoffice) or \
+        (df.BoxOffice.min(), df.BoxOffice.max())
+    movie_genre = selected_genre or None
+    director = input_director.strip() or None
+    cast_name = input_cast.strip() or None
+    x_axis = x_axis_var or 'Meter'
+    y_axis = y_axis_var or 'Reviews'
 
-    filtered_df = (df.pipe(lambda df: df[df['Reviews'] >= nb_reviews])
-                   .pipe(lambda df: df[df['Year'] >= year_released])
-                   .pipe(lambda df: df[df['Oscars'] >= oscars_won]))
+    filtered_df = (
+        df.pipe(lambda df: df[df['Reviews'] >= nb_reviews])
+        .pipe(lambda df: df[(df['Year'] >= year_released_start) & (df['Year'] <= year_released_end)])
+        .pipe(lambda df: df[df['Oscars'] >= oscars_won])
+        .pipe(lambda df: df[(df['BoxOffice'] >= dollars_boxoffice_min) & (df['BoxOffice'] <= dollars_boxoffice_max)])
+        .pipe(lambda df: df[df['Genre'].str.contains(movie_genre)] if movie_genre else df)
+        .pipe(lambda df: df[df['Director'].str.contains(director)] if director else df)
+        .pipe(lambda df: df[df['Cast'].str.contains(cast_name)] if cast_name else df)
+    )
 
     return {
         'data': [
             go.Scatter(
-                x=filtered_df[filtered_df.has_oscar == True].Meter,
-                y=filtered_df[filtered_df.has_oscar == True].Reviews,
-                text=filtered_df[filtered_df.has_oscar == True].Title,
+                x=filtered_df[filtered_df.has_oscar == True][x_axis],
+                y=filtered_df[filtered_df.has_oscar == True][y_axis],
+                text=filtered_df[filtered_df.has_oscar == True]['Title'],
                 mode='markers',
                 opacity=0.5,
                 marker={
@@ -127,9 +205,9 @@ def update_scatter_plot(selected_nb_reviews, selected_year_released, selected_nb
                 name='has_oscar: yes'
             ),
             go.Scatter(
-                x=filtered_df[filtered_df.has_oscar == False].Meter,
-                y=filtered_df[filtered_df.has_oscar == False].Reviews,
-                text=filtered_df[filtered_df.has_oscar == False].Title,
+                x=filtered_df[filtered_df.has_oscar == False][x_axis],
+                y=filtered_df[filtered_df.has_oscar == False][y_axis],
+                text=filtered_df[filtered_df.has_oscar == False]['Title'],
                 mode='markers',
                 opacity=0.5,
                 marker={
@@ -143,22 +221,62 @@ def update_scatter_plot(selected_nb_reviews, selected_year_released, selected_nb
         'layout': go.Layout(
             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
             xaxis={
-                'title': variable_labels['Meter'],
+                'title': variable_labels[x_axis],
                 'range': [
-                    filtered_df.Meter.min() - 10,
-                    filtered_df.Meter.max() + 10
+                    filtered_df[x_axis].min(),
+                    filtered_df[x_axis].max()
                 ]
             },
             yaxis={
-                'title': variable_labels['Reviews'],
+                'title': variable_labels[y_axis],
                 'range': [
-                    filtered_df.Reviews.min() - 10,
-                    filtered_df.Reviews.max() + 10
+                    filtered_df[y_axis].min(),
+                    filtered_df[y_axis].max()
                 ]},
             hovermode='closest'
         )
     }
 
+
+@app.callback(
+    dash.dependencies.Output('dataset-rows-p', 'children'),
+    [
+        dash.dependencies.Input('nb-reviews-slider', 'value'),
+        dash.dependencies.Input('year-released-range-slider', 'value'),
+        dash.dependencies.Input('oscars-won-slider', 'value'),
+        dash.dependencies.Input('dollars-boxoffice-range-slider', 'value'),
+        dash.dependencies.Input('genre-dropdown', 'value'),
+        dash.dependencies.Input('director-input', 'value'),
+        dash.dependencies.Input('cast-input', 'value'),
+        dash.dependencies.Input('x-axis-dropdown', 'value'),
+        dash.dependencies.Input('y-axis-dropdown', 'value')
+    ]
+)
+def update_nb_rows_selected(selected_nb_reviews, selected_years_released, selected_nb_oscars_won,
+                            selected_dollars_boxoffice, selected_genre, input_director, input_cast,
+                            x_axis_var, y_axis_var):
+    nb_reviews = selected_nb_reviews or df.Reviews.min()
+    year_released_start, year_released_end = selected_years_released or (df.Year.min(), df.Year.max())
+    oscars_won = selected_nb_oscars_won or df.Oscars.min()
+    dollars_boxoffice_min, dollars_boxoffice_max = (amount * 1e6 for amount in selected_dollars_boxoffice) or \
+        (df.BoxOffice.min(), df.BoxOffice.max())
+    movie_genre = selected_genre or None
+    director = input_director.strip() or None
+    cast_name = input_cast.strip() or None
+    x_axis = x_axis_var or 'Meter'
+    y_axis = y_axis_var or 'Reviews'
+
+    filtered_df = (
+        df.pipe(lambda df: df[df['Reviews'] >= nb_reviews])
+        .pipe(lambda df: df[(df['Year'] >= year_released_start) & (df['Year'] <= year_released_end)])
+        .pipe(lambda df: df[df['Oscars'] >= oscars_won])
+        .pipe(lambda df: df[(df['BoxOffice'] >= dollars_boxoffice_min) & (df['BoxOffice'] <= dollars_boxoffice_max)])
+        .pipe(lambda df: df[df['Genre'].str.contains(movie_genre)] if movie_genre else df)
+        .pipe(lambda df: df[df['Director'].str.contains(director)] if director else df)
+        .pipe(lambda df: df[df['Cast'].str.contains(cast_name)] if cast_name else df)
+    )
+
+    return 'Number of rows selected: {}'.format(len(filtered_df.index))
 
 # Add a custom CSS stylesheet
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
--- get_data.py
+++ get_data.py
@@ -1,5 +1,7 @@
-import pandas as pd
+import itertools as it
 import sqlite3
+
+import pandas as pd
 
 # Read SQLite results into a Pandas DataFrame
 with sqlite3.connect('movies.db') as con:
@@ -11,10 +13,20 @@ all_movies = pd.merge(omdb, tomatoes, how='inner', on='ID')\
     .pipe(lambda df: df[df['Reviews'] >= 10])\
     .assign(has_oscar=lambda row: row.Oscars >= 1)
 
+# Fill NaN values with empty string for categorical variables and
+# with 0. for numerical variable
+all_movies['Genre'].fillna(value='', inplace=True)
+all_movies['Director'].fillna(value='', inplace=True)
+all_movies['Cast'].fillna(value='', inplace=True)
+all_movies['BoxOffice'].fillna(value=.0, inplace=True)
 
+# Unique Genre values
+genres = set(list(it.chain.from_iterable([g.split(', ') for g in all_movies.Genre if g])))
+
+# Mapping between variables and labels for dropdown selection
 variable_labels = {
     'Meter': 'Tomato Meter',
-    'Rating': 'Numeric Rating',
+    'Rating_y': 'Numeric Rating',
     'Reviews': 'Number of reviews',
     'BoxOffice': 'Dollars at box office',
     'Year': 'Year',
